9a24b52465b4ade71c4191d47758e20f1277e2ff
kirukomaru11 <kiruko@duck.com>
2025-12-01 22:36:37 -0300

    main.py: use idle_add for play_button

--- main.py
+++ main.py
@@ -151,15 +151,12 @@ play_replace = regex("listview row:selected label:first-child {.*}")
 def play_button(*_):
     if not app.window.get_visible(): return
     color = "ffffff" if app.get_style_manager().get_dark() else "222226"
-    if player.get_playing():
-        svg = f"""<g fill="#{color}"><path d="m 3 1 h 3 c 0.550781 0 1 0.449219 1 1 v 12 c 0 0.550781 -0.449219 1 -1 1 h -3 c -0.550781 0 -1 -0.449219 -1 -1 v -12 c 0 -0.550781 0.449219 -1 1 -1 z m 0 0"/><path d="m 10 1 h 3 c 0.550781 0 1 0.449219 1 1 v 12 c 0 0.550781 -0.449219 1 -1 1 h -3 c -0.550781 0 -1 -0.449219 -1 -1 v -12 c 0 -0.550781 0.449219 -1 1 -1 z m 0 0"/></g>"""
-    else:
-        svg = f"""<path d="m 2 2.5 v 11 c 0 1.5 1.269531 1.492188 1.269531 1.492188 h 0.128907 c 0.246093 0.003906 0.488281 -0.050782 0.699218 -0.171876 l 9.796875 -5.597656 c 0.433594 -0.242187 0.65625 -0.734375 0.65625 -1.226562 c 0 -0.492188 -0.222656 -0.984375 -0.65625 -1.222656 l -9.796875 -5.597657 c -0.210937 -0.121093 -0.453125 -0.175781 -0.699218 -0.175781 h -0.128907 s -1.269531 0 -1.269531 1.5 z m 0 0" fill="#{color}"/>"""
+    svg = f"""<g fill="#{color}"><path d="m 3 1 h 3 c 0.550781 0 1 0.449219 1 1 v 12 c 0 0.550781 -0.449219 1 -1 1 h -3 c -0.550781 0 -1 -0.449219 -1 -1 v -12 c 0 -0.550781 0.449219 -1 1 -1 z m 0 0"/><path d="m 10 1 h 3 c 0.550781 0 1 0.449219 1 1 v 12 c 0 0.550781 -0.449219 1 -1 1 h -3 c -0.550781 0 -1 -0.449219 -1 -1 v -12 c 0 -0.550781 0.449219 -1 1 -1 z m 0 0"/></g>""" if player.get_playing() else f"""<path d="m 2 2.5 v 11 c 0 1.5 1.269531 1.492188 1.269531 1.492188 h 0.128907 c 0.246093 0.003906 0.488281 -0.050782 0.699218 -0.171876 l 9.796875 -5.597656 c 0.433594 -0.242187 0.65625 -0.734375 0.65625 -1.226562 c 0 -0.492188 -0.222656 -0.984375 -0.65625 -1.222656 l -9.796875 -5.597657 c -0.210937 -0.121093 -0.453125 -0.175781 -0.699218 -0.175781 h -0.128907 s -1.269531 0 -1.269531 1.5 z m 0 0" fill="#{color}"/>"""
     css.style = play_replace.sub("", css.style) + """listview row:selected label:first-child { background-image: url('data:image/svg+xml,<svg height="20px" viewBox="0 0 16 16" width="20px">""" + svg + "</svg>'); font-size: 0; background-repeat: no-repeat; background-position: 50%; opacity: 1; }"
     GLib.idle_add(css.load_from_string, css.style)
 player = Gtk.MediaFile.new()
-player.connect("notify::playing", play_button)
-app.get_style_manager().connect("notify::dark", play_button)
+player.connect("notify::playing", lambda *_: GLib.idle_add(play_button))
+app.get_style_manager().connect("notify::dark", lambda *_: GLib.idle_add(play_button))
 def filter_playlist(*_):
     e = lambda i: (sidebar.get_selected() == 3) or (sidebar.get_selected() == 0 and app.folder == artist_page.file and i.has_parent(app.folder)) or (app.folder != artist_page.file and i.has_prefix(app.folder))
     if sidebar.get_selected() in (4, 5):
